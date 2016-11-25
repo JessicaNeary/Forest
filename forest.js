a1f20a11ad25e58659abfb210a2023fb6a7f1410
Jess Neary <Caneary@hotmail.com>
2016-11-25 15:48:28 +1300

    Added walking animation and lights

--- forest.js
+++ forest.js
@@ -1,11 +1,11 @@
-document.onkeydown = move;
+document.onkeyup = move;
 
 function move(e){
 			console.log(e.keyCode);
 	if(e.keyCode==39){
 		walkRight();
 		//move forest right
-		shift(".forest", "-=10px");
+		shift(".forest", "-=15px");
 		shift("html", "-=30px");
 	}
 	else if(e.keyCode==37){
@@ -13,20 +13,52 @@ function move(e){
 		if(parseInt($(".forest").css('marginLeft')) < parseInt('-10px')){
 			walkLeft();
 			//move forest left
-			shift(".forest", "+=10px");
+			shift(".forest", "+=15px");
 			shift("html", "+=30px");
 		}
 	}
+	else if(e.keyCode==38){
+		walkFront();
+	}
+}
+
+function walkFront(){
+	$(".kiwi").css('background', 'url(images/kiwi/walkingFront.png)');
+	$(".kiwi").css('background-size', '400px');
+	$(".kiwi").css('top', '435px');
+	$(".kiwi").css('width', '80px');
+	$(".kiwi").css('height', '75px');
 }
 
 function walkLeft(){
 	$(".kiwi").css('background', 'url(images/kiwi/walkingLeft.png)');
 	$(".kiwi").css('background-size', '500px');
+	$(".kiwi").css('top', '450px');
+	$(".kiwi").css('width', '100px');
+	$(".kiwi").css('height', '60px');
+	step();
 }
 
 function walkRight(){
 	$(".kiwi").css('background', "url(images/kiwi/walkingRight.png)");
 	$(".kiwi").css('background-size', '500px');
+	$(".kiwi").css('top', '450px');
+	$(".kiwi").css('width', '100px');
+	$(".kiwi").css('height', '60px');
+	step();
+}
+
+var steps = 0;
+
+function step(){
+	if(steps == 0){
+		$(".kiwi").css('background-position', '-100px');
+		steps++;
+	}
+	else{
+	  $(".kiwi").css('background-position', '0px');
+		steps--;
+	}
 }
 
 function shift(item,pixels){
